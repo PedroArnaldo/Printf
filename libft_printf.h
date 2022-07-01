90c03bdabc1f8d4af472718faaa2fc2ff4a050d6
Pedro Arnaldo madeira lopes <parnaldo@c2t3s8.42.rio>
2022-07-01 19:55:03 -0300

    add makefile

--- libft_printf.h
+++ libft_printf.h
@@ -1,48 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   libft_printf.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: parnaldo <parnaldo@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/26 13:51:11 by parnaldo          #+#    #+#             */
-/*   Updated: 2022/07/01 19:22:40 by parnaldo         ###   ########.fr       */
+/*   Created: 2022/07/01 19:29:11 by parnaldo          #+#    #+#             */
+/*   Updated: 2022/07/01 19:39:23 by parnaldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef LIBFT_PRINTF_H
+# define LIBFT_PRINTF_H
+
+#include "libft/libft.h"
 #include <stdarg.h>
-#include "libft.h"
 
-int ft_printvar(const char c, va_list *var)
-{
-	if (c == 'c')
-	{
-	
-	}
-	else if (c == 's')
-	else if (c == '%')
-}
+int	ft_putchar(char c);
 
-int ft_printf(const char *str, ...)
-{
-	int i;
-	va_list args;
-	int print_len;
-	if(!str)
-		return (0);
-	i = 0;
-	while (str[i])
-	{
-		if(str[i] == '%')
-		{
-			print_len  += ft_printvar(str[i + 1], args);
-			i++;
-		}
-		else
-		{
-			print_len++;
-		}
-		i++;
-	}
-	return (print_len);
-}
+#endif
